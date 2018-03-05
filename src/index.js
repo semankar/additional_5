2d0be3ccb59290a4c9a378866d9090b0e9346b8f
semankar <semankar@mail.ru>
2018-03-05 18:29:19 +0300

    First commit, same brackets yet aren't working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack = [];
+  var oddSameBracket = false;
+  for (var i = 0; i < str.length; i++)
+  {
+    for (var j = 0; j < bracketsConfig.length; j++)
+    {      
+      if (bracketsConfig[j][0] == bracketsConfig[j][1] == str[i]) 
+      {
+        switch(oddSameBracket)
+        {
+          case false:
+            stack.push(j);
+            break;
+          case true:
+            if (stack.pop() != j)
+              return false; 
+              break;            
+        }     
+        oddSameBracket = !oddSameBracket;
+        continue;
+      }                 
+      if (str[i] == bracketsConfig[j][0]) //starting bracket
+      {
+        stack.push(j);
+        continue;
+      }
+      if (str[i] == bracketsConfig[j][1]) //ending bracket
+        if (stack.pop() != j)
+         return false;    
+    }
+  }
+  if ((stack.length > 0)||(oddSameBracket)) return false; 
+  return true;
 }
